727eeff94a6aee83f6a61c59444734be47b7b6e0
Josh Long <54473+joshlong@users.noreply.github.com>
2025-10-14 17:53:23 -0400

    adoptions

--- adoptions/src/main/java/com/example/demo/adoptions/Adoptions.java
+++ adoptions/src/main/java/com/example/demo/adoptions/Adoptions.java
@@ -2,19 +2,26 @@ package com.example.demo.adoptions;
 
 import org.springframework.beans.factory.BeanRegistrar;
 import org.springframework.beans.factory.BeanRegistry;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@Import(Adoptions.AdoptionsBeanRegistrar.class)
+//@Import(AdoptionsBeanRegistrar.class)
 class Adoptions {
 
-    static class AdoptionsBeanRegistrar implements BeanRegistrar {
+    @Bean
+    AdoptionsService adoptionsService (DogRepository repository , ApplicationEventPublisher publisher){
+        return new AdoptionsService(repository ,publisher);
+    }
+}
 
-        @Override
-        public void register(BeanRegistry registry, Environment env) {
-            registry.registerBean(AdoptionsService.class);
-        }
+class AdoptionsBeanRegistrar implements BeanRegistrar {
+
+    @Override
+    public void register(BeanRegistry registry, Environment env) {
+        registry.registerBean(AdoptionsService.class);
     }
 }
